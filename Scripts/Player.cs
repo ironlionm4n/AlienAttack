e706e69dd334ee82afb2241072000bf708e00c45
Mikail Miller <65094447+ironlionm4n@users.noreply.github.com>
2023-07-05 19:58:30 -0400

    character body 2D

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,15 +1,20 @@
 using Godot;
-using System;
+
+namespace AlienAttack.Scripts;
 
 public partial class Player : Node
 {
+	private CharacterBody2D _playerCharacterBody2D;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_playerCharacterBody2D = GetNode<CharacterBody2D>("root/Player");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		GD.Print(_playerCharacterBody2D.Name);
 	}
 }
